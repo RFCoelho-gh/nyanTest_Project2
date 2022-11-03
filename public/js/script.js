5f9f06d7c4c0a1e6b1fdfe9d2bedb72dab719a32
Rafael <rafael.f.coelho@outlook.com>
2022-11-03 11:58:53 +0000

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("nyanProject JS imported successfully!");
+});
